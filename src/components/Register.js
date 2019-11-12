0770072dc4ee40b547995cb8cd39ea6d6d86c4c1
Amy Fountain <36521868+amyfou@users.noreply.github.com>
2019-11-12 10:04:13 -0700

    Revert "added styling and broke register"

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,36 +5,34 @@ import { flowRight as compose } from 'lodash'
 import { addUserMutation, getUserToken } from '../queries/queries'
 import { handleLoginUser } from '../actions/users'
 import { hashToArray } from '../utils/helpers'
-import { Button, Grid, Header, Message, Segment, Input } from 'semantic-ui-react';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 
 class Register extends Component {
-  _isMounted = false
-  constructor(props) {
-    super(props);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    //set default appearance to register rather than login
-    this.state = {
-      login: false,
-      first: '',
-      last: '',
-      username: '',
-      email: '',
-      password: '',
-      roles: '',
-    }
+  state = {
+    first: '',
+    last: '',
+    username: '',
+    email: '',
+    password: '',
+    roles: '',
   }
 
   handleInputChange = (e) => {
     const { value, name } = e.target
+
     this.setState(() => ({
       [name]: value
     }))
   }
+  isDisabled = () => {
+    const { first, last, username, email, password } = this.state
 
-  onFormSubmit = (e) => {
+    return first === ''
+      || last === ''
+      || username === ''
+      || email === ''
+      || password === ''
+  }
+  handleSubmit = (e) => {
     e.preventDefault()
     console.log('I am logging in')
     this.props.history.push('/')
@@ -42,168 +40,60 @@ class Register extends Component {
   }
 
 render() {
-  const { first, last, username, email, password, roles, login } = this.state
-  const addUserSchema = Yup.object().shape({
-    first: Yup.string()
-      .required('Required'),
-    last: Yup.string()
-      .required('Required'),
-    username: Yup.string()
-      .required('Required'),
-    email: Yup.string()
-      .email('Please enter a valid email address')
-      .required('Required'),
-    password: Yup.string()
-      .min(2, 'Password must be more than 2 characters')
-      .max(30, 'Password must be less than 30 characters')
-      .required('Required'),
-    passwordConfirmation: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match')
-      .required('Password confirmation is required!')
-    });
-
-  const loginSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('Please enter a valid email address')
-      .required('Required'),
-    password: Yup.string()
-      .min(2, 'Password must be more than two characters')
-      .max(30, 'Password must be less than 30 characters!')
-      .required('Required'),
-    });
-
+  const { first, last, username, email, password, roles } = this.state
     return (
-      <Grid textAlign='center'  verticalAlign='top'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as='h2'  textAlign='center'>
-              {login ? 'Log in to your account' : 'Create an account'}
-          </Header>
-          {this.state.error && (
-           <Message className="error">Unsuccessful: {this.state.error}</Message>
-          )}
-          <Segment stacked>
-            <Formik
-              initialValues={{ first: '', last: '', username: '', email: '', password: '', passwordConfirmation: ''}}
-              validationSchema={ !login ? addUserSchema : loginSchema }
-              onSubmit={(values, { setSubmitting }) => {
-                this.onFormSubmit(values, setSubmitting);
-              }}
-            >
-              {({ isSubmitting, values, errors, touched, handleChange, handleBlur }) => (
-            <Form>
-              {!login && (
-                <Input
-                  fluid
-                  icon="write"
-                  iconPosition="left"
-                  id="first"
-                  placeholder="First Name"
-                  type="text"
-                  value={ values.first }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  className={ errors.first && touched.first ? 'text-input error' : 'text-input' }
-                />
-                )}
-                {errors.first && touched.first && !login && (
-                  <div className="input-feedback">{errors.first}</div>
-                )}
-              {!login && (
-                <Input
-                  fluid
-                  icon="write"
-                  iconPosition="left"
-                  id="last"
-                  placeholder="Last Name"
-                  type="text"
-                  value={ values.last }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  className={ errors.last && touched.last ? 'text-input error' : 'text-input' }
-                />
-              )}
-                {errors.last && touched.last && !login && (
-                <div className="input-feedback">{errors.last}</div>
-                )}
-              {!login && (
-                <Input
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  id="username"
-                  placeholder="Username"
-                  type="text"
-                  value={ values.username }
-                  onChange={ handleChange }
-                  onBlur={ handleBlur }
-                  className={ errors.username && touched.username ? 'text-input error' : 'text-input' }
-                />
-              )}
-                {errors.username && touched.username && !login && (
-                <div className="input-feedback">{errors.username}</div>
-                )}
-              <Input
-                fluid
-                icon="mail"
-                iconPosition="left"
-                id="email"
-                placeholder="email"
-                type="text"
-                value={ values.email }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.email && touched.email ? 'text-input error' : 'text-input'}
-              />
-                {errors.email && touched.email && (
-                <div className="input-feedback">{errors.email}</div>
-                )}
-              <Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                id="password"
-                placeholder="Password"
-                type="password"
-                value={ values.password }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.password && touched.password ? 'text-input error' : 'text-input' }
-              />
-                {errors.password && touched.password && (
-                <div className="input-feedback">{errors.password}</div>
-                )}
-            {!login && (
-              <Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                id="passwordConfirmation"
-                placeholder="Confirm your password"
-                type="password"
-                value={ values.passwordConfirmation }
-                onChange={ handleChange }
-                onBlur={ handleBlur }
-                className={ errors.passwordConfirmation && touched.passwordConfirmation ? 'text-input error' : 'text-input' }
-              />
-              )}
-                {errors.passwordConfirmation && touched.passwordConfirmation && !login &&(
-                <div className="input-feedback">{errors.passwordConfirmation}</div>
-                )}
-              <Button color="blue" size="large" type="submit" disabled={isSubmitting}>
-                Submit
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </Segment>
-          <Message>
-            <Button color="black" onClick={() => this.setState({ login: !login })}>
-                {login ? 'need to create an account?' : 'already have one?'}
-            </Button>
-          </Message>
-      </Grid.Column>
-    </Grid>
-   );
+      <form className='add-form' onSubmit={this.handleSubmit}>
+        <h3 style={{marginBottom: 5}}>Create an Account</h3>
+        <label className='label' htmlFor='first'>First Name</label>
+        <input
+          value={first}
+          onChange={this.handleInputChange}
+          name='first'
+          className='input'
+          id='first'
+          type='text'
+        />
+        <label className='label' htmlFor='last'>Last Name</label>
+        <input
+          value={last}
+          onChange={this.handleInputChange}
+          name='last'
+          className='input'
+          id='last'
+          type='text'
+        />
+        <label className='label' htmlFor='email'>Email</label>
+        <input
+          value={email}
+          onChange={this.handleInputChange}
+          name='email'
+          className='input'
+          id='email'
+          type='text'
+        />
+        <label className='label' htmlFor='username'>Username</label>
+        <input
+          value={username}
+          onChange={this.handleInputChange}
+          name='username'
+          className='input'
+          id='username'
+          type='text'
+        />
+        <label className='label' htmlFor='password'>Password</label>
+        <input
+          value={password}
+          onChange={this.handleInputChange}
+          name='password'
+          className='input'
+          id='password'
+          type='text'
+        />
+      <button className='btn' type='submit' disabled={this.isDisabled()}>
+        Submit
+      </button>
+    </form>
+    )
   }
 }
 
